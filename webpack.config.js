8060afd211e8b1098c2b6226be7652db6f366ff3
David Lane <me@dvdln.com>
2016-11-29 13:40:58 -0500

    Fix Babel config

--- webpack.config.js
+++ webpack.config.js
@@ -36,13 +36,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            ['es2015', {modules: false}],
-            'stage-2'
-          ]
-        }
+        loader: 'babel-loader'
       }
     ]
   },
