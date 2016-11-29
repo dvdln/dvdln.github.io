efd5481883585ea3bedf37e7a9a411c91a8ed836
David Lane <me@dvdln.com>
2016-11-29 11:52:38 -0500

    Move script tag in App to appease XO

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,15 +1,15 @@
+<script>
+  export default {
+    name: 'app'
+  };
+</script>
+
 <template>
   <div>
     <h1>David Lane</h1>
     <div>I use things to make stuff</div>
   </div>
 </template>
-
-<script>
-  export default {
-    name: 'app'
-  };
-</script>
 
 <style>
   @import '~minireset.css';
