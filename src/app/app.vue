364907083fc1a86c55db5bfd938af8db93886c3e
David Lane <me@dvdln.com>
2016-11-30 14:12:35 -0500

    Move text-size-adjust rule

--- src/app/app.vue
+++ src/app/app.vue
@@ -40,6 +40,7 @@
     font-weight: 400;
     font-size: 16px;
     line-height: 1.5;
+    -webkit-text-size-adjust: none;
   }
 
   body {
@@ -50,7 +51,6 @@
   @media only screen and (max-width : 480px) {
     body {
       padding: 1rem;
-      -webkit-text-size-adjust: none;
     }
   }
 
