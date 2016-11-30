62f15a569db6d1875dedfb6f9159e76f8e07248d
David Lane <me@dvdln.com>
2016-11-30 14:47:36 -0500

    Broke navigation into its own component

---
+++ src/app/app-navigation.vue
@@ -0,0 +1,51 @@
+<script>
+  import routes from '../routes';
+
+  const menu = routes.filter(r => r.nav);
+
+  export default {
+    name: 'app',
+    data: () => ({menu})
+  };
+</script>
+
+<template>
+  <nav>
+    <ul>
+      <router-link v-for="item in menu"
+                   :to="item.path"
+                   tag="li">
+        <a>{{item.nav.text}}</a>
+      </router-link>
+    </ul>
+  </nav>
+</template>
+
+<style>
+  nav {
+    margin: 1rem 0;
+    border: 0 solid lightgray;
+    border-top-width: 1px;
+  }
+
+  nav li {
+    display: inline-block;
+    margin: 0 1rem 0 0;
+    border: 0 solid transparent;
+    border-bottom-width: 3px;
+    transition: border-color 0.2s;
+  }
+
+  nav li.router-link-active {
+    border-color: currentColor;
+  }
+
+  nav li a {
+    display: inline-block;
+    margin: 0 -0.5rem;
+    padding: 0 0.5rem;
+    line-height: 3;
+    font-weight: 400;
+    text-decoration: none;
+  }
+</style>
--- src/app/app.vue
+++ src/app/app.vue
@@ -1,11 +1,11 @@
 <script>
-  import routes from '../routes';
-
-  const menu = routes.filter(r => r.nav);
+  import AppNavigation from './app-navigation.vue';
 
   export default {
     name: 'app',
-    data: () => ({menu})
+    components: {
+      AppNavigation
+    }
   };
 </script>
 
@@ -16,15 +16,7 @@
       <div>I use things to make stuff</div>
     </header>
 
-    <nav>
-      <ul>
-        <router-link v-for="item in menu"
-                     :to="item.path"
-                     tag="li">
-          <a>{{item.nav.text}}</a>
-        </router-link>
-      </ul>
-    </nav>
+    <app-navigation></app-navigation>
 
     <main>
       <transition name="fade">
@@ -83,32 +75,6 @@
     font-size: 2.5rem;
     font-weight: 800;
     line-height: 1;
-  }
-
-  nav {
-    margin: 1rem 0;
-    border: 0 solid lightgray;
-    border-top-width: 1px;
-  }
-
-  nav li {
-    display: inline-block;
-    margin: 0 1rem 0 0;
-    border: 0 solid transparent;
-    border-bottom-width: 3px;
-    transition: border-color 0.2s;
-  }
-
-  nav li.router-link-active {
-    border-color: currentColor;
-  }
-
-  nav li a {
-    display: inline-block;
-    margin: 0 -0.5rem;
-    padding: 0 0.5rem;
-    line-height: 3;
-    text-decoration: none;
   }
 
   main {
